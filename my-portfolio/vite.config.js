ea522919ddb97bd9175d6a1a1a8cbf693b9b4ad3
Abigail Muthoni Wairi <89918147+Abbymuso1@users.noreply.github.com>
2024-01-28 21:38:54 +0300

    Feat: Adding on the libraries to deploy the website

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Reactified-Assignment-1-Portfolio-Page-EST/"
+  base: "/Reactified-Assignment-1-Portfolio-Page-EST/",
+  plugins: [react()]
 })
